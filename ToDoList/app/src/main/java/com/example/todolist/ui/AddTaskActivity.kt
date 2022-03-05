f321c12b1e00a52fb5726c59fcdbb5a35becd172
Shackluryz <lucas.shackluryz@gmail.com>
2022-03-05 14:34:10 -0300

    [ ADD ] Implementa os metodos para alterar e deletar tasks

--- ToDoList/app/src/main/java/com/example/todolist/ui/AddTaskActivity.kt
+++ ToDoList/app/src/main/java/com/example/todolist/ui/AddTaskActivity.kt
@@ -2,7 +2,6 @@ package com.example.todolist.ui
 
 import android.app.Activity
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.todolist.databinding.ActivityAddTaskBinding
 import com.example.todolist.datasource.TaskDataSource
@@ -22,6 +21,15 @@ class AddTaskActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAddTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if(intent.hasExtra(TASK_ID)) {
+            val taskId = intent.getIntExtra(TASK_ID, 0)
+            TaskDataSource.findById(taskId)?.let {
+                binding.tilTitle.text = it.title
+                binding.tilDate.text = it.date
+                binding.tilHour.text = it.hour
+            }
+        }
 
         insertListeners()
     }
@@ -55,11 +63,16 @@ class AddTaskActivity : AppCompatActivity() {
             val task = Task(
                 title = binding.tilTitle.text,
                 date = binding.tilDate.text,
-                hour = binding.tilHour.text
+                hour = binding.tilHour.text,
+                id = intent.getIntExtra(TASK_ID, 0)
             )
             TaskDataSource.insertTask(task)
             setResult(Activity.RESULT_OK)
             finish()
         }
+    }
+
+    companion object {
+        const val TASK_ID = "task_id"
     }
 }
