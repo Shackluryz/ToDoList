84edf780e3b17ef91906b4049a84a56d9fd612e3
Shackluryz <lucas.shackluryz@gmail.com>
2022-03-05 17:43:11 -0300

    [ ADD ][ UPDATE ] Atualização do metodo para iniciar a activity e incrementação da interface inicial

--- ToDoList/app/src/main/java/com/example/todolist/ui/MainActivity.kt
+++ ToDoList/app/src/main/java/com/example/todolist/ui/MainActivity.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.todolist.databinding.ActivityMainBinding
 import com.example.todolist.datasource.TaskDataSource
 
@@ -11,6 +13,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val adapter by lazy { TaskListAdapter() }
+
+
+    private val register = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) updateList()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,16 +28,18 @@ class MainActivity : AppCompatActivity() {
         updateList()
 
         insertListeners()
+        //Data Store
+        //Room
     }
 
     private fun insertListeners() {
         binding.fab.setOnClickListener {
-            startActivityForResult(Intent(this, AddTaskActivity::class.java), CREATE_NEW_TASK)
+            register.launch(Intent(this, AddTaskActivity::class.java))
         }
         adapter.listenerEdit = {
             val intent = Intent(this, AddTaskActivity::class.java)
             intent.putExtra(AddTaskActivity.TASK_ID, it.id)
-            startActivityForResult(intent, CREATE_NEW_TASK)
+            register.launch(intent)
         }
         adapter.listenerDelete = {
             TaskDataSource.deleteTask(it)
@@ -38,16 +47,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == CREATE_NEW_TASK && resultCode == Activity.RESULT_OK) updateList()
-    }
-
     private fun updateList() {
-        adapter.submitList(TaskDataSource.getList())
-    }
-
-    companion object {
-        private const val CREATE_NEW_TASK = 1000
+        val list = TaskDataSource.getList()
+        binding.includeEmpty.emptyState.visibility = if(list.isEmpty()) View.VISIBLE else View.GONE
+        adapter.submitList(list)
     }
 }
